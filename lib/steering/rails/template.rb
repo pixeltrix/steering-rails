33af77dfe90b969540905968870cbb9f21ea4456
Andrew White <andyw@pixeltrix.co.uk>
2012-09-19 05:19:32 +0100

    Remove whitespace

--- lib/steering/rails/template.rb
+++ lib/steering/rails/template.rb
@@ -6,14 +6,14 @@ module Steering
       def self.engine_initialized?
         defined? Steering.compile
       end
-  
+
       def initialize_engine
         require_template_library "steering"
       end
-  
+
       def prepare
       end
-  
+
       def evaluate(scope, locals, &block)
         "Handlebars.template(#{Steering.compile(data)})"
       end
